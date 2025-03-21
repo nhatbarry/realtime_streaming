2915fed24c9a1b6b6b490cbd1efe0317303a2928
nhatbarry <nhatbarry@gmail.com>
2025-03-21 19:00:06 +0700

    format data

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -9,14 +9,30 @@ from airflow.operators.python import PythonOperator
 
 def get_data():
     import requests
+    import json
     
     response = requests.get('https://randomuser.me/api/')
     response = response.json()
     return response['results'][0]
 
-def stream_data():
-    import json
+def format_data(response):
+    data = {}
+    data['name'] = f"{response['name']['first']} {response['name']['last']}"
+    data['gender'] = response['gender']
+    data['age'] = response['dob']['age']
+    data['dob'] = response['dob']['date']
+    data['username'] = response['login']['username']
+    data['password'] = response['login']['password']
+    data['address'] = f"{response['location']['street']['number']} {response['location']['street']['name']}, {response['location']['city']}, {response['location']['state']}, {response['location']['country']}"
+    data['email'] = response['email']
+    data['phone'] = response['phone']
+    data['picture'] = response['picture']['medium']
     
+    return data
+
+def stream_data():
+    res = get_data()
+    print(format_data(res))
 
 # with DAG('user_automation',
 #             default_args=default_args,
